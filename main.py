2ff84d4b869a0858ddcba9d3839671de76f7c70c
Sangram3101 <sangram.samanta@tuteck.com>
2025-08-01 20:25:20 +0530

    change in file structure

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
+print(f"Using Supabase URL: {SUPABASE_URL}")
+print(f"Using Supabase Key: {SUPABASE_KEY[:5]}...")  # only print prefix
+
 app = FastAPI()
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
